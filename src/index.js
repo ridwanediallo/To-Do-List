f62c41b638e40eb8270a6fa3bb57c9a564b023fb
Ridwan <ridwan>
2022-02-17 17:49:57 +0000

    Add add todo fonctionality

--- src/index.js
+++ src/index.js
@@ -1,34 +1,89 @@
 import './style.css';
 
 const list = document.querySelector('.list');
+const inputTodo = document.querySelector('.add-todo');
+const addTodoBtn = document.querySelector('.submit');
 
-const todoList = [
-  {
-    id: 0,
-    do: 'wash the dishes',
-    boolean: false,
-  },
-  {
-    id: 1,
-    do: 'complete to do list project',
-    boolean: false,
-  },
-  {
-    id: 2,
-    do: 'fix care',
-    boolean: false,
-  },
-];
 
-for (let i = 0; i < todoList.length; i += 1) {
-  list.innerHTML = ` ${list.innerHTML}
-        <li class="item">
-        <div class="left-itmes">
-        <input type="checkbox" class="checkbox">
-          <p id=${todoList[i].id} class="text">${todoList[i].do}</p>
-        </div>
-        <i class="fa-solid fa-ellipsis-vertical"></i>
-      </li>
-      <hr>
-        `;
+
+
+
+
+
+class TodoTask {
+  constructor(desp, index, done) {
+    this.desp = desp;
+    this.index = index;
+    this.done = done;
+  }
 }
+
+
+class TaskLisk {
+  constructor(){
+    this.tasks = [
+      {desp: 'go out'},
+      {desp: 'go out'},
+      {desp: 'go out'},
+      {desp: 'go out'},     
+    ];
+  }
+
+  saveTolocal(){
+	  localStorage.setItem('tasks', JSON.stringify(this.tasks));
+}
+
+getFromLocal = () => {
+  localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+}
+
+
+renderTodo = (list) => {
+  this.tasks.forEach((el, i) => {
+
+   const item = document.createElement('li'); 
+   const itemsLeft = document.createElement('div'); 
+   const inputCheck = document.createElement('input'); 
+   const par = document.createElement('p'); 
+   const dots = document.createElement('i')
+
+   item.classList.add('item');
+   itemsLeft.classList.add('left-itmes');
+
+   inputCheck.classList.add('checkbox');
+   inputCheck.setAttribute('type', 'checkbox')
+
+   par.classList.add('text');
+   par.textContent = el.desp;
+  //  dots.classList.add('fa-solid fa-ellipsis-vertical');
+
+   itemsLeft.append(inputCheck, par);
+   item.append(itemsLeft, dots);
+
+
+   list.appendChild(item);
+
+
+    // list.innerHTML = ` ${list.innerHTML}
+    //   <li class="item">
+    //   <div class="left-itmes">
+    //   <input type="checkbox" class="checkbox">
+    //     <p id=${i} class="text">${el}</p>
+    //   </div>
+    //   <i class="fa-solid fa-ellipsis-vertical"></i>
+    // </li>
+    // <hr>
+    //   `;
+  })
+}
+
+}
+
+const myTasks = new TaskLisk();
+
+myTasks.renderTodo(list);
+
+
+
+
+
