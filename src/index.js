f90555d41cdd8dc03b09059150cb9ca22903fbac
Ridwan <ridwan>
2022-02-22 13:58:15 +0000

    Add clearAll method

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import './style.css';
+import "./style.css";
 
-const list = document.querySelector('.list');
-const inputTodo = document.querySelector('.add-todo');
-const addTodoBtn = document.querySelector('.submit');
+const list = document.querySelector(".list");
+const inputTodo = document.querySelector(".add-todo");
+const addTodoBtn = document.querySelector(".submit");
+const clearList = document.querySelector(".clear");
 
 class TodoTask {
   constructor(desp, arr, done = false) {
@@ -12,32 +13,34 @@ class TodoTask {
   }
 }
 
-class TaskLisk {
+class TaskList {
   constructor() {
     this.tasks = [];
   }
 
   saveTolocal() {
-    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    localStorage.setItem("tasks", JSON.stringify(this.tasks));
   }
 
   getFromLocal = () => {
-    this.tasks = localStorage.getItem('tasks')
-      ? JSON.parse(localStorage.getItem('tasks'))
+    this.tasks = localStorage.getItem("tasks")
+      ? JSON.parse(localStorage.getItem("tasks"))
       : [];
   };
 
   renderTodo = (list) => {
-    list.innerHTML = '';
+    console.log(this.tasks);
+    list.innerHTML = "";
     this.tasks.forEach((el, i) => {
-      const item = document.createElement('li');
-      item.classList.add('item');
+      console.log(el);
+      const item = document.createElement("li");
+      item.classList.add("item");
       item.id = i;
       item.innerHTML = `
           <div class="left-itmes">
-          <input type="checkbox" class="checkbox" ${el.done ? 'checked' : ''}>
+          <input type="checkbox" class="checkbox" ${el.done ? "checked" : ""}>
           <input type='text' class='todo ${
-            el.done ? 'line-through' : ''
+            el.done ? "line-through" : ""
           }' value='${el.desp}'>
           </div>
           <div>
@@ -46,35 +49,35 @@ class TaskLisk {
           </div>
     `;
 
-      item.addEventListener('click', () => {
-        const iconRemove = item.querySelector('.icon-remove');
-        const iconDots = item.querySelector('.icon-dots');
-        iconRemove.classList.toggle('hidden');
-        iconDots.classList.toggle('hidden');
-        iconRemove.addEventListener('click', (event) => {
+      item.addEventListener("click", () => {
+        const iconRemove = item.querySelector(".icon-remove");
+        const iconDots = item.querySelector(".icon-dots");
+        iconRemove.classList.toggle("hidden");
+        iconDots.classList.toggle("hidden");
+        iconRemove.addEventListener("click", (event) => {
           const taskItem = event.target.parentNode.parentNode;
           this.removeItem(taskItem);
         });
       });
 
-      item.style.borderBottom = '1px solid #aaa';
+      item.style.borderBottom = "1px solid #aaa";
 
-      const checkBox = item.querySelector('.checkbox');
-      const todo = item.querySelector('.todo');
-      todo.classList.add('todo-style');
-      checkBox.addEventListener('click', this.clickCheck);
+      const checkBox = item.querySelector(".checkbox");
+      const todo = item.querySelector(".todo");
+      todo.classList.add("todo-style");
+      checkBox.addEventListener("click", this.clickCheck);
 
       list.append(item);
     });
   };
 
   addTodo(desp, tasks) {
-    if (desp !== '') {
+    if (desp !== "") {
       const todoItems = new TodoTask(desp, tasks);
       this.tasks.push(todoItems);
     }
 
-    inputTodo.value = '';
+    inputTodo.value = "";
   }
 
   clickCheck = (e) => {
@@ -82,7 +85,7 @@ class TaskLisk {
     const div = e.target.parentNode;
     const { id } = div.parentNode;
     this.tasks[id].done = !this.tasks[id].done;
-    todo.classList.toggle('line-through');
+    todo.classList.toggle("line-through");
     this.saveTolocal();
   };
 
@@ -99,18 +102,35 @@ class TaskLisk {
     const idItem = item.id;
     this.removeFromLocal(idItem);
   }
+
+  clearAll() {
+    this.tasks = this.tasks.filter((t) => console.log(t));
+
+    this.saveTolocal();
+    this.getFromLocal();
+    // renderTodo(list);
+  }
 }
 
-const myTasks = new TaskLisk();
+const myTasks = new TaskList();
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   myTasks.getFromLocal(myTasks.tasks);
   myTasks.renderTodo(list);
 });
 
-addTodoBtn.addEventListener('click', (e) => {
+addTodoBtn.addEventListener("click", (e) => {
   e.preventDefault();
   myTasks.addTodo(inputTodo.value, myTasks.tasks);
   myTasks.renderTodo(list);
   myTasks.saveTolocal();
 });
+
+const deleteCompletedTodo = () => {
+  clearList.addEventListener("click", () => {
+    myTasks.clearAll();
+    renderTodo(list);
+  });
+};
+
+deleteCompletedTodo();
